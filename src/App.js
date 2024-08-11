a5cd0dd0bc2300c78d932f9563bd6dd7d0db28f7
deepank joshi <deepsdj37@gmail.com>
2024-08-11 09:11:13 +0530

    getting all starter files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import "./index.css";
+function App() {
+  return (
+    <>
+      <div className="app">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
